87fafda72dfbf2faa21f164a01664dab76547a44
Tobias Holger Lauxmann <tobias_holger.lauxmann@student.reutlingen-university.de>
2022-02-25 19:32:19 +0100

    improved settingsMenu

--- Assets/Scripts/SettingsMenuView.cs
+++ Assets/Scripts/SettingsMenuView.cs
@@ -2,12 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
+using TMPro;
 
 public class SettingsMenuView : View
 {
     [SerializeField] private Button _backButton;
+    [SerializeField] private AudioMixer _audioMixer;
+    [SerializeField] private TMP_Dropdown _resolutionDropdown;
+
+
+
+    private Resolution[] _resolutions;
+    private void Start()
+    {
+        addScreenResolutionToDropdown();
+    }
+
     public override void Initialize()
     {
         _backButton.onClick.AddListener(() => ViewManager.ShowLast());
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = _resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetVolume(float volume)
+    {
+        _audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    private void addScreenResolutionToDropdown()
+    {
+        _resolutions = Screen.resolutions;
+        _resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < _resolutions.Length; i++)
+        {
+            string option = _resolutions[i].width + " x " + _resolutions[i].height;
+            Debug.Log(option);
+            options.Add(option);
+            
+            if(_resolutions[i].width == Screen.currentResolution.width &&
+                _resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        _resolutionDropdown.AddOptions(options);
+        _resolutionDropdown.value = currentResolutionIndex;
+        _resolutionDropdown.RefreshShownValue();
     }
 }
